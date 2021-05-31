549f3923de251ccd099df7da00aa98890c59934a
natLF3RD <38551443+natLF3RD@users.noreply.github.com>
2021-06-01 09:47:35 +1000

    Update q10.c

--- week 1/q10.c
+++ week 1/q10.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
+void recurse(int i);
 
 int main(void)
 {
     int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for (int i = 0; i < 10; i++) {
-        printf("%d\n", nums[i]);
-    }
+//     for (int i = 0; i < 10; i++) {
+//         printf("%d\n", nums[i]);
+//     }
+    
+    recurse(0);
     return 0;
 }
 
-int recurse (int i, int *nums) {
-    // exit condition 
-    if (i>=10) {
-        return 1;
-    }
-
-    // code 
+void recurse(int i) {
+    // check to exit / exit condition -> return once it fails
+    if (i >= 10) return;
+    
+    // do stuff
     printf("%d\n", nums[i]);
-
-    // change of state
+    
+    // change any variables to reach exit eventually
     i++;
-
-    // recursion
-    recurse(i, nums);
+    // recurse / call self again
+    recurse(i);
 }
 
 
 
-int aaa () {
-    int i=0;
 
-    while (i<10) {
-        // do ur thing.
-        i++;
-    }
-    int i;
 
-    for (intialise; check/ condition ; incrememnt) {
 
-    }
+// for loop
+int i=0;
+while (i<10) {
+    // do stuff
+    i++;
+}
+int i;
+for (i=0; i<10; i++) {
+    
 }
